21f30f5f31781ab0cf9ebb8251ae306e680bb3b9
ccwscott <ccwscottgithub@gmail.com>
2016-08-21 21:42:15 -0700

    Shows all data

--- jsonCSharp/Controllers/DataController.cs
+++ jsonCSharp/Controllers/DataController.cs
@@ -13,22 +13,7 @@ namespace jsonCSharp.Controllers
         public ActionResult getPrices()
         {
             Models.Location currentLocationData = new Models.Location();
-            currentLocationData.locationName = "Earth";
-            currentLocationData.metalPrice = "15";
-            currentLocationData.powerPrice = "53";
-            currentLocationData.refugees = "15";
-            currentLocationData.research = "1000";
-            currentLocationData.waterPrice = "0";
-            currentLocationData.airPrice = "0";
-            currentLocationData.distance = 4000;
-            currentLocationData.foodPrice = "4";
-            currentLocationData.fuelPrice = "5";
-            currentLocationData.weaponPrice = "50";
-            currentLocationData.nearbyLocations = new string[4];
-            currentLocationData.nearbyLocations[0] = "Luna";
-            currentLocationData.nearbyLocations[1] = "Mars";
-            currentLocationData.nearbyLocations[2] = "F3";
-            currentLocationData.nearbyLocations[3] = "Earth";
+            currentLocationData = Models.ServerData.getCurrentLocationData();
             return Json(currentLocationData, JsonRequestBehavior.AllowGet);
         }
 	}
--- jsonCSharp/Models/data.cs
+++ jsonCSharp/Models/data.cs
@@ -19,18 +19,59 @@ namespace jsonCSharp.Models
         public string refugees;
         public string research;
         public int distance;
-        public string[] nearbyLocations;
+        public List<string> nearbyLocations;
     }
 
     public class ServerData
     {
-        public Location getCurrentLocationData()
+        static public Location getCurrentLocationData()
         {
+            Location currentLocationData = new Location();
+
+            //open connection to server
             SqlConnection sqlConnection = new SqlConnection("Server=tcp:newgaia.database.windows.net,1433;Database=Newgaia;User ID=anticlockwisegames@newgaia;Password=K1i2l3l4A5l6l7H8u9m0a1n2s3;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;");
             sqlConnection.Open();
-            string sqlString = "((SELECT * FROM PT_Location INNER JOIN PT_PilotData ON PT_PilotData.Location = PT_Location.ID) INNER JOIN PT_Sales ON PT_Sales.Location =  PT_Location.ID) INNER_JOIN PT_Items on PT_Sales.Item = PT_Items.ID)";
+
+            //get prices
+            string sqlString = "SELECT PT_Items.Name as itemName, PT_Sales.BuyPrice FROM PT_Locations INNER JOIN PT_PilotData ON PT_PilotData.Location = PT_Locations.ID INNER JOIN PT_Sales ON PT_Sales.Location =  PT_Locations.ID INNER JOIN PT_Items on PT_Sales.Item = PT_Items.ID";
             SqlCommand sqlCommand = new SqlCommand(sqlString, sqlConnection);
-            Location currentLocationData = new Location();
+            SqlDataReader itemPrices = sqlCommand.ExecuteReader();
+            while(itemPrices.Read())
+            {
+                if ((string) itemPrices["itemName"] == "Air")
+                    currentLocationData.airPrice = itemPrices["BuyPrice"].ToString();
+                else if ((string)itemPrices["itemName"] == "Power")
+                    currentLocationData.powerPrice = itemPrices["BuyPrice"].ToString();
+                else if ((string)itemPrices["itemName"] == "Water")
+                    currentLocationData.waterPrice = itemPrices["BuyPrice"].ToString();
+                else if ((string)itemPrices["itemName"] == "Food")
+                    currentLocationData.foodPrice = itemPrices["BuyPrice"].ToString();
+                else if ((string)itemPrices["itemName"] == "Fuel")
+                    currentLocationData.fuelPrice = itemPrices["BuyPrice"].ToString();
+                else if ((string)itemPrices["itemName"] == "Weapons")
+                    currentLocationData.weaponPrice = itemPrices["BuyPrice"].ToString();
+                else if ((string)itemPrices["itemName"] == "Metal")
+                    currentLocationData.metalPrice = itemPrices["BuyPrice"].ToString();
+                else if ((string)itemPrices["itemName"] == "Refugees")
+                    currentLocationData.refugees = itemPrices["BuyPrice"].ToString();
+                else if ((string)itemPrices["itemName"] == "Research")
+                    currentLocationData.research = itemPrices["BuyPrice"].ToString();
+            }
+
+            itemPrices.Close();
+
+            currentLocationData.nearbyLocations = new List<string>();
+
+            //get nearby locations
+            sqlString = "SELECT PT_Locations.Name as Name FROM PT_Locations INNER JOIN PT_PilotData on PT_Locations.ID != PT_PilotData.Location";
+            sqlCommand = new SqlCommand(sqlString, sqlConnection);
+            SqlDataReader nearbyLocations = sqlCommand.ExecuteReader();
+
+            while (nearbyLocations.Read())
+            {
+                currentLocationData.nearbyLocations.Add((string) nearbyLocations["Name"]);
+            }
+
             return currentLocationData;
         }
     }
